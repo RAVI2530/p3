f6a625c1a5524633baa19a4312a33df95ae7cd76
RAVI2530 <ravi.patil911@gmail.com>
2020-07-21 19:08:54 +0530

    templates added

--- p3/views.py
+++ p3/views.py
@@ -9,3 +9,13 @@ def home(request):
 
 def second(request):
     return render(request,"directory/second.html")
+
+def third(request):
+    return render(request,"directory/third.html",context={'data':"abc",'name':"Ravi"})
+
+def fourth(request):
+    fruits=['apple','mango','banana','kiwi','orange']
+    return render(request,"directory/fourth.html",{'fruits':fruits})
+
+def fifth(request):
+    return render(request,"directory/fifth.html",{'a':-100,'b':-5})
